dbf77b18cafc1dc10e4051b1de6c742b3d71b416
Ashot Avagyan <avagyanah@gmail.com>
2023-05-21 21:58:12 +0400

    .parcelrc config

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -1,14 +1,17 @@
 import { Localization, type ILocalizationOptions } from '@gameastic/localization';
 (async () => {
     const config: ILocalizationOptions = {
-        path: './assets/',
+        path: './locales/',
         fallback: 'en',
         supported: ['en', 'ru'],
         mapping: { ru: ['hy', 'ge'] },
     };
 
     await Localization.init(config);
-    await Localization.setLanguage('ru');
 
-    console.warn(Localization.get('hello'));
+    await Localization.setLanguage('en');
+    console.info(`${Localization.language} => ${Localization.get('hello')}`);
+
+    await Localization.setLanguage('ru');
+    console.info(`${Localization.language} => ${Localization.get('hello')}`);
 })();
