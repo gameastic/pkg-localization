dcdfc92109dd9dd4544c9039f36d91c4222d79bb
Ashot Avagyan <avagyanah@gmail.com>
2023-05-21 04:31:55 +0400

    added tests

--- packages/localization/test/localization.test.ts
+++ packages/localization/test/localization.test.ts
@@ -1,3 +1,29 @@
-test('localization', () => {
-    //
+import { Localization, type ILocalizationOptions } from '../src';
+
+const config: ILocalizationOptions = {
+    path: { en: { hello: 'Hello' }, ru: { hello: 'Привет' } },
+    fallback: 'en',
+    supported: ['en', 'ru'],
+    mapping: { ru: ['hy', 'ge'] },
+};
+
+test('localization language', async () => {
+    await Localization.init(config);
+    await Localization.setLanguage('ru');
+
+    expect(Localization.get('hello')).toEqual('Привет');
+});
+
+test('localization fallback', async () => {
+    await Localization.init(config);
+    await Localization.setLanguage('fr');
+
+    expect(Localization.get('hello')).toEqual('Hello');
+});
+
+test('localization mapping', async () => {
+    await Localization.init(config);
+    await Localization.setLanguage('hy');
+
+    expect(Localization.get('hello')).toEqual('Привет');
 });
